1756fbfe4f5dc9a9b3c0f7b67d52cf6be97e1323
indraneel sarode <indraneel.sarode@coditas.com>
2024-05-27 17:11:35 +0530

    made changes for sql prompt

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 
-
 import os
 from dotenv import load_dotenv
 load_dotenv()
 from langchain_community.utilities import SQLDatabase
 from langchain_groq import ChatGroq
-from operator import itemgetter
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from langchain.chains import create_sql_query_chain
-from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_core.prompts import MessagesPlaceholder
+
 
 
 groq_api_key = os.getenv("groq_api_key")
@@ -37,73 +32,39 @@ db = connect_to_database()
 
 
 
-system = """You are a {dialect} expert. Given an input question, creat a syntactically correct {dialect} query to run.
-Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per {dialect}. You can order the results to return the most informative data in the database.
-Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
-Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
-Pay attention to use date('now') function to get the current date, if the question involves "today".
+system = """   - You are an SQL  expert.
+            - Create a syntactically correct {dialect} query. 
+            - You have access to the following tables: {table_names}.
+            - Your first job is to execute the query according to the user input.
+           
+            ##INSTRUCTIONS##
+            
+            1. Your response should be in table format only nothing else.
+            
+            2. The column names should be in the same order as the SQL query.
+            3. Strictly, the column names are human-readable but with the same meaning as SQL column have. 
+            (Example: OPENING_AMT = Opening Amount)
+  
+            **NOTE**
+            1. DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.)
+            
+            
+            HUMAN:
+            {input}
 
-Only use the following tables:
-{table_info}
-
-Write an initial draft of the query. Then double check the {dialect} query for common mistakes, including:
-- Using NOT IN with NULL values
-- Using UNION when UNION ALL should have been used
-- Using BETWEEN for exclusive ranges
-- Data type mismatch in predicates
-- Properly quoting identifiers
-- Using the correct number of arguments for functions
-- Casting to the correct data type
-- Using the proper columns for joins
-
-Use format:
-
-First draft: <<FIRST_DRAFT_QUERY>>
-Final answer: <<FINAL_ANSWER_QUERY>>
-
- #Important# 
-        1. Your response should be only the SQL Query. 
-        2. In the response there should be no other words than the sql query.
-        3. Don't write '\' in response
 """
 
 
 prompt = ChatPromptTemplate.from_messages(
-    [("system", system), ("human", "{input}")]
+    [("system", system), ("human", "{input}"),(MessagesPlaceholder(variable_name="agent_scratchpad"))]
 ).partial(dialect=db.dialect)
 
-
-def parse_final_answer(output: str) -> str:
-    return output.split("Final answer: ")[1]
-
-
-write_chain = create_sql_query_chain(llm, db, prompt=prompt) | parse_final_answer
-
-execute_query = QuerySQLDataBaseTool(db=db)
-
-
-answer_prompt = PromptTemplate.from_template(
-            """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
-
-            Question: {question}
-            SQL Query: {query}
-            SQL Result: {result}
-            Answer: """
-        )
-
-
-answer = answer_prompt | llm | StrOutputParser() 
-
-chain = (
-    RunnablePassthrough.assign(query=write_chain).assign(
-        result=itemgetter("query") | execute_query
-    )
-    | answer
-) 
-
+agent_executor = create_sql_agent(llm, db=db,agent_type="openai-tools", prompt=prompt, verbose=True)
 
 while True:
     user_input = input("enter the query: ")
-    if user_input == "" or user_input=="exit":
+    if user_input=="exit" or user_input=="":
         break
-    print(chain.invoke({"question":user_input}))
+    response=agent_executor.invoke(user_input)
+
+    print(response['output'])
